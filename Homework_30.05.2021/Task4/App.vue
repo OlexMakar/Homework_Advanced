f807ec339b2272bb414969d11c89fe7d4d2d603d
Olexandr <79477441+OlexMakar@users.noreply.github.com>
2021-05-31 02:16:10 +0300

    Add files via upload

---
+++ Homework_30.05.2021/Task4/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <converter/>
+  </div>
+</template>
+
+<script>
+import converter from './components/converter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    converter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
