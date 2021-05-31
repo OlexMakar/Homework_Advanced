ad38978ecb2456efecb02758d5c9153b09e76bde
OlexMakar <olexmakar@gmail.com>
2021-06-01 01:44:48 +0300

    added task2

---
+++ Homework 2/Task 2/Prediction.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>
+      <button @click="getPrediction">Отримати передбачення!</button>
+    </div>
+    <div>
+      <span>
+        {{ yourPrediction }}
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "prediction",
+  data() {
+    return {
+      yourPrediction: null,
+      predictionArray: [
+        "Удача прийде звідки не чекаєте.",
+        "Всі незакінчені справи будуть завершені.",
+        "Яскрава пригода вже чекає вас.",
+        "Вас чекає несподіване грошове надходження.",
+      ],
+    };
+  },
+  methods: {
+    getRandom(min, max) {
+      return min + Math.floor(Math.random() * (max - min + 1));
+    },
+    getPrediction() {
+      const randIndex = this.getRandom(0, this.predictionArr.length - 1);
+      this.yourPrediction = this.predictionArr[randIndex];
+    },
+  },
+};
+</script>
+<style lang="scss" scoped></style>
