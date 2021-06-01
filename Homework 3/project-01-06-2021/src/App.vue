ddc5ea712c9c2a09100376e7bc9177614796de16
OlexMakar <olexmakar@gmail.com>
2021-06-02 01:23:45 +0300

    -

---
+++ Homework 3/project-01-06-2021/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <product-card
+      v-for="(item, index) in productList"
+      :key="index"
+      :image-src="item.imageSrc"
+      :product-title="item.productTitle"
+      :product-link="item.productLink"
+      :product-price="item.productPrice"
+      :is-in-store="item.isInStore"
+    />
+    <!-- <product-card
+      image-src="https://content.rozetka.com.ua/goods/images/preview/11314684.jpg"
+      product-title="Тример універсальний PHILIPS Series 5000"
+      product-link="https://bt.rozetka.com.ua/ua/philips_mg5730_15/p24852941/"
+      product-price="1599 грн"
+    />
+    <product-card
+      imageSrc="https://content.rozetka.com.ua/goods/images/preview/101601326.png"
+      productTitle="MSI PCI-Ex GeForce GTX 1660 Super Gaming X 6GB GDDR6 (192bit)
+    (1830/14000) (HDMI, 3 x DisplayPort) (GTX 1660 SUPER GAMING X)"
+      productLink="https://hard.rozetka.com.ua/ua/msi_gtx_1660_super_gaming_x/p143436890/"
+      productPrice="8599 грн"
+    /> -->
+  </div>
+</template>
+
+<script>
+import ProductCard from "./components/ProductCard.vue";
+
+export default {
+  name: "App",
+  components: {
+    ProductCard,
+  },
+  data() {
+    return {
+      productList: [
+        {
+          id: 1,
+          imageSrc:
+            "https://content.rozetka.com.ua/goods/images/preview/11314684.jpg",
+          productTitle: "Тример універсальний PHILIPS Series 5000",
+          productLink:
+            "https://bt.rozetka.com.ua/ua/philips_mg5730_15/p24852941/",
+          productPrice: "1599 грн",
+          isInStore: false,
+        },
+        {
+          id: 2,
+          imageSrc:
+            "https://content.rozetka.com.ua/goods/images/preview/101601326.png",
+          productTitle:
+            "MSI PCI-Ex GeForce GTX 1660 Super Gaming X 6GB GDDR6 (192bit) (1830/14000) (HDMI, 3 x DisplayPort) (GTX 1660 SUPER GAMING X)",
+          productLink:
+            "https://hard.rozetka.com.ua/ua/msi_gtx_1660_super_gaming_x/p143436890/",
+          productPrice: "8599 грн",
+          isInStore: true,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  align-items: flex-start;
+}
+</style>
---
+++ Homework 3/project-01-06-2021/src/components/ProductCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div class="image">
+      <a :href="productLink">
+        <img :src="imageSrc" alt="product" />
+      </a>
+    </div>
+    <div>
+      <a :href="productLink">{{ productTitle }}</a>
+    </div>
+    <div>
+      {{ productPrice }}
+    </div>
+    <div>
+      {{ isAvailable }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  //Дані, необхідні для створення компонента:
+  props: {
+    imageSrc: {
+      type: String,
+      default: "https://bytes.ua/wp-content/uploads/2017/08/no-image.png",
+    },
+    productTitle: {
+      type: String,
+      default: "no title",
+    },
+    productLink: {
+      type: String,
+      default: "https://rozetka.com.ua/ua/",
+    },
+    productPrice: {
+      type: [Number, String],
+      default: 0,
+    },
+    isInStore: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {
+    isAvailable() {
+      return this.isInStore ? "У наявності" : "Немає у наявності";
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.container {
+  color: black;
+  display: inline-block;
+  height: 300px;
+  width: 150px;
+  border: 2px solid blue;
+  border-radius: 5px;
+}
+</style>
